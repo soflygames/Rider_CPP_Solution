38f5ff8c3ccb3836218031426b2ca209803b58a2
So <124211622+soflygames@users.noreply.github.com>
2024-04-30 14:01:29 -0500

    saved

--- Rider C++ Template.cpp
+++ Rider C++ Template.cpp
@@ -1,8 +1,21 @@
-
 #include <iostream>
+#include <stdlib.h>
+
+
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
-}
+    system("CLS");
 
+    
+    int var = 5;
+    int*ptr = &var;
+    
+    std::cout << "Value of var: " << var << std::endl;
+    std::cout << "Derefferenced of *var: " << *ptr << std::endl;
+    std::cout << "value of ptr: " << ptr << std::endl;
+    std::cout << "value of &var " << &var << std::endl;
+    std::cout << "value of &ptr " << &ptr << std::endl;
+    //std::cout << "value of *var " << *var << std::endl;
+}
